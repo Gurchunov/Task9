6ab87b6c4bb59aa850913c5fcf38127e650857cd
gurchunov-1997@mail.ru <Flatronl1718s>
2022-03-26 19:12:12 +0300

    сделал чтение и запись

--- src/com/company/teachmeskills/Main.java
+++ src/com/company/teachmeskills/Main.java
@@ -1,8 +1,21 @@
 package com.company.teachmeskills;
 
+import java.io.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("src/com/company/teachmeskills/input.txt"));
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("src/com/company/teachmeskills/output.txt"));
+            String s;
+            while ((s = bufferedReader.readLine()) != null) {
+                bufferedWriter.write(s+"\n");
+                bufferedWriter.flush();
+            }
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
     }
 }
